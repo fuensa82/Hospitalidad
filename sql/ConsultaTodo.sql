4221a679520b8b00686a345be9b4914410b10ba8
vPalomo <vPalomo@AytoFuensaP008.fuensalida.com>
2022-06-03 13:27:58 +0200

    Terminada version estable

--- sql/ConsultaTodo.sql
+++ sql/ConsultaTodo.sql
@@ -1,10 +1,13 @@
 
 SELECT personas.Nombre, personas.Apellidos, personas.Localidad, autobuses.Descripcion AS Bus, hoteles.NombreHotel AS hotel, habitaciones.Descripcion1 AS habitacion
-FROM personas, habitaciones, hoteles, autobuses, relpersonaautobus, relpersonahabitacion, relviajetodo
+FROM personas, habitaciones, hoteles, autobuses, relpersonaautobus, relpersonahabitacion, relviajetodo, viajes
 WHERE habitaciones.idHotel=hoteles.IdHotel AND
 	personas.idPersona=relviajetodo.idPersona AND
 	relpersonahabitacion.idPersona=personas.idPersona AND
 	relpersonahabitacion.idHabitacion=habitaciones.idHabitacion AND
 	habitaciones.idHotel=hoteles.IdHotel AND
 	autobuses.idAutobus=relpersonaautobus.idAutobus AND
-	personas.idPersona=relpersonaautobus.idPersona
+	personas.idPersona=relpersonaautobus.idPersona and
+	relviajetodo.idViaje=viajes.idViaje AND 
+	relviajetodo.idViaje='1'
+ORDER BY personas.Apellidos, personas.Nombre
